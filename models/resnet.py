97755541215e13011ffe946508b7d0f5ce7457a9
Hui Kang <hui.kang@sap.com>
2019-03-18 15:18:42 +0800

    Update resnet

--- models/resnet.py
+++ models/resnet.py
@@ -1,32 +1,33 @@
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
+from collections import OrderedDict
 
 
 __all__ = ['resnet50', 'resnet101']
 
 
-def conv1x1(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+def conv1x1(inputs, outputs, stride=1):
+    return nn.Conv2d(inputs, outputs, kernel_size=1, stride=stride, bias=False)
 
 
-def conv3x3(in_channels, out_channels, stride=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+def conv3x3(inputs, outputs, stride=1):
+    return nn.Conv2d(inputs, outputs, kernel_size=3, stride=stride, padding=1, bias=False)
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_channels, channels, stride=1, shortcut=None):
+    def __init__(self, inputs, planes, stride=1, shortcut=None):
         super(Bottleneck, self).__init__()
         self.residual = nn.Sequential(
-            conv1x1(in_channels, channels),
-            nn.BatchNorm2d(channels),
+            conv1x1(inputs, planes),
+            nn.BatchNorm2d(planes),
             nn.ReLU(inplace=True),
-            conv3x3(channels, channels, stride),
-            nn.BatchNorm2d(channels),
+            conv3x3(planes, planes, stride),
+            nn.BatchNorm2d(planes),
             nn.ReLU(inplace=True),
-            conv1x1(channels, channels * self.expansion),
-            nn.BatchNorm2d(channels * self.expansion)
+            conv1x1(planes, planes * self.expansion),
+            nn.BatchNorm2d(planes * self.expansion)
         )
         self.shortcut = shortcut
 
@@ -42,52 +43,41 @@ class Bottleneck(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, layers, classes=1000):
         super(ResNet, self).__init__()
-        self.in_channels = 64
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False), # /2
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1) # /2
-        )
-        self.layer2 = self.make_layers(block, 64,  layers[0], stride=1)
-        self.layer3 = self.make_layers(block, 128, layers[1], stride=2) # /2
-        self.layer4 = self.make_layers(block, 256, layers[2], stride=2) # /2
-        self.layer5 = self.make_layers(block, 512, layers[3], stride=2) # /2
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, classes)
+        self.inputs = 64
+        self.features = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)), # /2
+            ('norm1', nn.BatchNorm2d(64)),
+            ('relu1', nn.ReLU(inplace=True)),
+            ('pool1', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)), # /2
+            ('conv2', self.make_layers(block, 64, layers[0], stride=1)),
+            ('conv3', self.make_layers(block, 128, layers[1], stride=2)), # /2
+            ('conv4', self.make_layers(block, 256, layers[2], stride=2)), # /2
+            ('conv5', self.make_layers(block, 512, layers[3], stride=2)), # /2
+            ('pool2', nn.AdaptiveAvgPool2d((1, 1)))
+        ]))
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+        self.classifier = nn.Linear(512 * block.expansion, classes)
 
-    def make_layers(self, block, channels, blocks, stride=1):
+    def make_layers(self, block, planes, blocks, stride=1):
         shortcut = None
-        if stride != 1 or self.in_channels != channels * block.expansion:
+        if stride != 1 or self.inputs != planes * block.expansion:
             shortcut = nn.Sequential(
-                conv1x1(self.in_channels, channels * block.expansion, stride),
-                nn.BatchNorm2d(channels * block.expansion)
+                conv1x1(self.inputs, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion)
             )
 
         layers = []
-        layers.append(block(self.in_channels, channels, stride, shortcut))
-        self.in_channels = channels * block.expansion
+        layers.append(block(self.inputs, planes, stride, shortcut))
+        self.inputs = planes * block.expansion
         for _ in range(1, blocks):
-            layers.append(block(self.in_channels, channels))
+            layers.append(block(self.inputs, planes))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.avgpool(x)
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.classifier(x)
 
         return x
 
